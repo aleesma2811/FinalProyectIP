30cedd7c38cbe173427e42cabbd27bba7f1c9948
efren <efrenfloresp@gmail.com>
2023-11-22 15:12:06 -0600

    Update, changed style and made to files

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ProyectIP
  This repository will contain the code made for the final proyect of the class of Image Processing
 
-requirements: 
-pygame, numpy, cv2 (openCV)
---
+++ mainCode.py
@@ -0,0 +1,39 @@
+# mainCode.py
+
+import cv2
+import numpy as np
+
+def transform_to_dog_sight(image):
+    # Replace this with actual transformation logic for dog sight
+    # For now, just return the original image
+    return image
+
+def transform_to_cat_sight(image):
+    # Replace this with actual transformation logic for cat sight
+    # For now, just return the original image
+    return image
+
+def transform_to_bird_sight(image):
+    # Replace this with actual transformation logic for bird sight
+    # For now, just return the original image
+    return image
+
+def transform_to_bee_sight(image):
+    # Replace this with actual transformation logic for bee sight
+    # For now, just return the original image
+    return image
+
+def transform_to_bat_sight(image):
+    # Replace this with actual transformation logic for bat sight
+    # For now, just return the original image
+    return image
+
+def transform_to_crab_sight(image):
+    # Replace this with actual transformation logic for crab sight
+    # For now, just return the original image
+    return image
+
+def transform_to_snake_sight(image):
+    # Replace this with actual transformation logic for snake sight
+    # For now, just return the original image
+    return image
